80f67455d7f588a3f19f0e87057fad18e70e7e47
cotyadams <cotyadams1@gmail.com>
2024-02-08 16:27:05 -0500

    new

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const uuid = require('uuid');
+const { v4: uuid } = require('uuid');
 // creating object
 const server = express();
 
@@ -17,22 +17,43 @@ server.use(express.json());
 // data array
 let data = [
     {
-        id: uuid, // String, required
+        id: uuid(), // String, required
         name: "Jane Doe",  // String, required
         bio: "Having fun", // String, required
     },
+    {
+        id: uuid(), // String, required
+        name: "johnny",  // String, required
+        bio: "Having fun", // String, required
+    }
 ]
 
 // request handlers
-server.get('/api', (req, res) => {
-    console.log('get')
-    res.status(200).json(data)
+// get handlers
+server.get('/api/users', (req, res) => {
+    res.status(200).json(data);
 })
 
-// server.post('/api/users', (req, res) => {
-//     data.push({id: uuid})
-//     res.status(201).json(data)
-// })
+server.get('/api/users/:id', (req, res) => {
+    res.status(200).json(data.find((user) => (user.id === req.params.id)));
+})
+
+// post handlers
+server.post('/api/users', (req, res) => {
+    data.push({ id: uuid(), name: req.body.name, bio: req.body.bio });
+    res.status(201).json(data);
+})
+
+// delete handlers
+server.delete('/api/users/:id', (req, res) => {
+    let deletedUser = {};
+    data = [...data.filter((user) => {
+        if (user.id === req.params.id) deletedUser = user;
+        return user.id !== req.params.id;
+    })]
+    console.table(data)
+    res.status(200).json(deletedUser)
+})
 
 server.listen(8000, () => {console.log('api running')})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
